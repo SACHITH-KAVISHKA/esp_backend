3f210339da7ddf27a266f03acc3ca126131899d0
Sachith <172572870+SACHITH-KAVISHKA@users.noreply.github.com>
2026-01-02 10:42:43 +0530

    Refactor MongoDB configuration to use environment variables for connection details

--- app.py
+++ app.py
@@ -31,8 +31,8 @@ CORS(app, resources={r"/*": {"origins": "*"}})
 socketio = SocketIO(app, cors_allowed_origins="*", async_mode='threading')
 
 # MongoDB Configuration
-MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://sachithkavishka24_db_user:9AreY8oheuYOFpjZ@cluster0.it8iznp.mongodb.net/?appName=Cluster0")
-DB_NAME = os.getenv("DB_NAME", "bus_fleet_db")
+MONGO_URI = os.getenv("MONGO_URI")
+DB_NAME = os.getenv("DB_NAME", "bus_speed_predict_api")
 
 try:
     mongo_client = MongoClient(MONGO_URI, serverSelectionTimeoutMS=5000)
